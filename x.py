86fce807fc90964d269b63c8ce8486a29db408f3
kallepallysaikiran <134003319+kallepallysaikiran@users.noreply.github.com>
2023-06-03 20:15:43 +0530

    Add files via upload

---
+++ x.py
@@ -0,0 +1,28 @@
+
+import sqlite3
+
+conn = sqlite3.connect('database.db')
+cursor = conn.cursor()
+cursor = conn.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS students(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    rollno INTEGER,
+    email TEXT,
+    contact INTEGER,
+    branch TEXT
+);''')
+               
+cursor.execute('''CREATE TABLE IF NOT EXISTS faculties(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    faculty_id INTEGER,
+    email TEXT,
+    phone_number INTEGER,
+    qualifications TEXT
+);''')
+x=9898
+y=989989
+conn.commit()
+
+conn.close()
